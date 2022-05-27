e9ac0010043af49739252b7b6be8ac566f755e9f
Daniel92222 <benitezblancooscardaniel@gmail.com>
2022-05-26 20:27:03 -0500

    Login

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,8 @@
 var express = require('express');
 var router = express.Router();
+const data = require('../userData');
+const methods = require('../methods');
+
 
 
 //rutas
@@ -12,58 +15,83 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'COMPANY PROGRAMMING KHO' });
 });
 
+
+router.get ('/home', function(req, res, next){
+  res.render ('home',{title1: "Bienvenido"});
+})
 router.get('/register', (req, res) => {
   res.render(registerR);
 });
 
+router.get('/login', (req, res) => {
+  res.render(loginR);
+});
+
 
 //REGISTER
-router.post('/register',async (req, res) => {
+
+router.post('/register', (req, res) => {
   const {fulln, email, password, cp } = req.body;
-
-  try{
-    if (password === cp){
-      
-       //validar si el correo existe
-       user = await User.findOne({email: email})
-       .then(user => {
-         if(user){
-           res.render(registerR,{
-             message: "El usuario ya esta registrado",
-             messageClass: "alert-danger"
-           });
-         }else{
-           const hashedPassword = methods.getHashedPassWord(password);
-
-           const userDB = new User({'fulln': fulln, 'email':email, 'password': hashedPassword})
-
-           userDB.save();
-
-           res.render(loginR,{
-            message: "El registro se ha completado",
-            messageClass: "alert-success"
-          });
-         }
-       });
-   
-   
-     }else{
-       res.render(registerR,{
-         message: "El password no coincide",
-         messageClass: "alert-danger"
-       });
-       
-     }
   
-  }
-  catch(error){
-    console.log('error', error);
+  if (password === cp){
+    if (data.data.find(dat => dat.email === email)) {
+      res.render(registerR,{
+        message: "El usuario ya esta registrado",
+        messageClass: "alert-danger"
+      });
+
+    }
+
+    const hashedPassword = methods.getHashedPassWord(password);
+
+    // actualizamos el array con el nuevo registro
+ 
+    data.data.push({
+      fulln,
+      email,
+      password: hashedPassword
+    });
+  
+    res.render(loginR,{
+      message: "El registro se ha completado",
+      messageClass: "alert-success"
+    });
+
+  }else{
+    res.render(registerR,{
+      message: "El password no coincide",
+      messageClass: "alert-danger"
+    });
     
   }
+
 });
 
 //LOGIN
 
+
+router.post ('/login', (req, res) => {
+  const {email, password } = req.body;
+  const hashedPassword = methods.getHashedPassWord(password);
+
+  const dataUser = data.data.find(u => {
+    return u.email === email && hashedPassword === u.password;
+  });
+
+  if (dataUser){
+    const authToken = methods.generateAuthToken();
+
+    methods.authTokens[authToken] = dataUser;
+    res.cookie('AuthToken', authToken);
+    res.redirect("/home");
+
+  }else{
+    res.render(loginR, {
+      message: "Usuario y password invalidos",
+      messageClass: "alert-danger"
+    });
+  }
+});
 
 
 
